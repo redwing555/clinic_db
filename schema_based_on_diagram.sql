b15f6595b9074219e480ad23f57b5ad1489ec4c7
ntzwilly <ntzwilly@gmail.com>
2021-12-07 15:55:20 +0200

    Create table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,5 +5,17 @@ CREATE DATABASE clinic;
 CREATE TABLE patients (
     id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY(id)
 );
+
+
+CREATE TABLE medical_histories (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    patient_id INT,
+    CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients (id),
+    PRIMARY KEY (id)
+);
+
